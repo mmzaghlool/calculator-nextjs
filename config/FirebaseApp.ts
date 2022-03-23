129d9a26b8378962abd9b6b8cb39d44fabbf67cf
Mostafa Mahmoud <mmzaghlool52@gmail.com>
2022-03-23 22:16:39 +0200

    Update env links vars

--- config/FirebaseApp.ts
+++ config/FirebaseApp.ts
@@ -2,24 +2,24 @@
 import Firebase, { initializeApp, getApps } from 'firebase/app';
 import { Analytics, isSupported, getAnalytics, logEvent } from 'firebase/analytics';
 
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
-const REACT_APP_AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
-const REACT_APP_PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
-const REACT_APP_STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
-const REACT_APP_SENDER_ID = process.env.REACT_APP_SENDER_ID;
-const REACT_APP_APP_ID = process.env.REACT_APP_APP_ID;
-const REACT_APP_MEASUREMENT_ID = process.env.REACT_APP_MEASUREMENT_ID;
+const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const NEXT_PUBLIC_AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
+const NEXT_PUBLIC_PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
+const NEXT_PUBLIC_STORAGE_BUCKET = process.env.NEXT_PUBLIC_STORAGE_BUCKET;
+const NEXT_PUBLIC_SENDER_ID = process.env.NEXT_PUBLIC_SENDER_ID;
+const NEXT_PUBLIC_APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const NEXT_PUBLIC_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID,
+  apiKey: NEXT_PUBLIC_API_KEY,
+  authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_SENDER_ID,
+  appId: NEXT_PUBLIC_APP_ID,
+  measurementId: NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 class FirebaseApp {
@@ -33,7 +33,7 @@ class FirebaseApp {
 
     if (apps.length > 0) {
       this._app = apps[0];
-    } else if (REACT_APP_API_KEY !== undefined) {
+    } else if (NEXT_PUBLIC_API_KEY !== undefined) {
       this._app = initializeApp(firebaseConfig);
     }
 
